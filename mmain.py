ca948c36af2999384cdfedf2560250a997634a9a
Isam H <isamlund@hotmail.com>
2023-03-01 20:53:21 +0100

    hhhhhh

---
+++ mmain.py
@@ -0,0 +1,16 @@
+def main():
+    name = input("Enter your name: ")
+    print(f'name = {name}')
+    k=1    
+    w= 9  
+    x= 4
+    y= 8
+    z= 77
+    s =3
+    d= 5
+    tt = z -x + y -s - d
+    v=44
+    print(tt +v)
+
+if __name__=='__main__':
+    main()
